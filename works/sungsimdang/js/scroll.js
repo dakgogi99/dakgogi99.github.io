a8861b6f58bb5bbed806261e86d3511750fdcacd
dakgogi99 <73578858+dakgogi99@users.noreply.github.com>
2020-10-31 12:20:46 +0900

    Update scroll.js

--- works/sungsimdang/js/scroll.js
+++ works/sungsimdang/js/scroll.js
@@ -15,10 +15,10 @@ $(function(){
   //브라우저 높이값 == section 높이값
   $section.height(winH);
   //브라우저 크기가 바뀌면 == section 높이값
-  $(window).on('resize', function(){
+  /*$(window).on('resize', function(){
     $section.height($(window).height());
   });
-  
+  */
   $gnb.on('click', 'li a', function(e){
     
     var winH = $(window).height(),
