e7818bd330ef64f7ba53876f0a449089b7d11591
dakgogi99 <73578858+dakgogi99@users.noreply.github.com>
2020-11-13 14:21:32 +0900

    Update bodyluv.js

--- works/bodyluv/js/bodyluv.js
+++ works/bodyluv/js/bodyluv.js
@@ -77,14 +77,14 @@ $(function(){
     }
     
     /*멈추게 하는 함수 만들기*/      
-    function stopTimer () {
+    /*function stopTimer () {
       clearInterval(timer)
     }
     $slideBox
       .on({
         mouseenter: stopTimer,
         mouseleave: startTimer
-      })
+      })*/
     
     /*함수 실행(호출)*/
     slideShow(currentIndex);
@@ -170,14 +170,14 @@ $(function(){
     }
     
     /*멈추게 하는 함수 만들기*/      
-    function stopTimer () {
+   /* function stopTimer () {
       clearInterval(timer)
     }
     $slideBox
       .on({
         mouseenter: stopTimer,
         mouseleave: startTimer
-      })
+      })*/
     
     /*함수 실행(호출)*/
     slideShow(currentIndex);
